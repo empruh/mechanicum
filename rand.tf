85f7abf7aa99fd59fff5f180b2093eb675214837
empruh <bosc.vivien@gmail.com>
2017-10-24 12:35:00 +0200

    all set now

--- rand.tf
+++ rand.tf
@@ -6,3 +6,6 @@ resource "aws_instance" "trololol" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+resource "aws_instance" "ololol" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
